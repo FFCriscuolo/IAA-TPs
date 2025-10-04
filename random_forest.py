4af66082262888783bf81831d9742abadf3c37bd
FFCriscuolo <facundocriscuolo@gmail.com>
2025-10-04 12:59:20 -0300

    Cambio DT

--- random_forest.py
+++ random_forest.py
@@ -6,12 +6,14 @@ def bootsrap(X, y):
     indices = np.random.choice(n_samples, size=n_samples, replace=True)
     return X[indices], y[indices] 
 
-def RandomForestList (X, y, n_trees=10, max_depth=None, min_samples_split=2, min_samples_leaf=1, max_features=5):
+def RandomForestList (X, y, n_trees=10, max_depth=None, min_samples_split=2, min_samples_leaf=1, max_features=5, bootsrap=False):
     trees_list = []
     for _ in range(n_trees):
-        X_sample, y_sample = bootsrap(X, y)
-        #X_sample= X
-        #y_sample= y
+        if bootsrap:
+            X_sample, y_sample = bootsrap(X, y)
+        else:
+            X_sample= X
+            y_sample= y
         tree = CART(max_depth=max_depth, min_samples_split=min_samples_split, min_samples_leaf=min_samples_leaf)
         tree.rndm_forest = True
         tree.max_features = max_features
